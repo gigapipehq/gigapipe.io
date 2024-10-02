9152aaef5fe8c1855eb13007fe299256bccb8d0f
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-03 00:23:46 +0200

    Update rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
+  description: 'Gigapipe: Polyglot Observability',
   root: 'docs',
   base: '/homepage/',
     title: 'Gigapipe',
