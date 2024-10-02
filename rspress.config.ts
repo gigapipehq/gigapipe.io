c27320fadc4bf0a4ecb4547b94b147e601e76c71
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-03 00:05:03 +0200

    Add themeConfig

--- rspress.config.ts
+++ rspress.config.ts
@@ -9,4 +9,19 @@ export default defineConfig({
     light: "/gigapipe-logo-color.svg",
     dark: "/Gigapipe-logo-outline@3x.svg",
   },
+  themeConfig: {
+    darkMode: true,
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/gigapipehq',
+      },
+      {
+        icon: 'linkedin',
+        mode: 'link',
+        content: 'https://www.linkedin.com/company/gigapipe',
+      },
+    ],
+  },
 });
