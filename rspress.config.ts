247e640c8db80724711d5ad66e9e9900663776b0
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-03 11:53:53 +0200

    Image style

--- rspress.config.ts
+++ rspress.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
         content: 'https://www.linkedin.com/company/gigapipe',
       },
       {
-        icon: { svg: `<image src="https://raw.githubusercontent.com/gigapipehq/homepage/refs/heads/main/docs/public/qryn.png"/>`},
+        icon: { svg: `<image src="https://raw.githubusercontent.com/gigapipehq/homepage/refs/heads/main/docs/public/qryn.png" style="filter: grayscale(1);"/>`},
         mode: 'link',
         content: 'https://blog.qryn.dev',
       },
