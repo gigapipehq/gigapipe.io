77cf6382c9387bcc1a91951d16f1c81b6e92cc46
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-04 13:03:10 +0200

    Update rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -18,15 +18,15 @@ export default defineConfig({
     sidebar: {
       '/docs/docs/':[
         {
-          text: "Getting Started",
+          text: "Documentation",
           items: [
             {
-              text: "Demo",
-              link: '/docs/demo'
+              text: "Get Started",
+              link: '/docs'
             },
             {
-              text: "Home",
-              link: '/docs'
+              text: "Explore Data",
+              link: '/docs/demo'
             }
           ]
           
