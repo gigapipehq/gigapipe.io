74541c3f050d17a984f636ffa2876be24b461274
Joel Guerra <jacovinus@gmail.com>
2024-10-04 15:36:16 +0200

    fix: main page

---
+++ src/components/docs/Docs.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { HomeHero, HomeFeature } from "rspress/theme";
+
+
+const Docs = () => {
+    return (
+        <div
+         style={{position:'absolute'}}
+         >
+            <HomeHero
+            
+                frontmatter={{
+                    title: "Gigapipe Home",
+                    titleSuffix: "Polyglot Observability",
+                    hero: {
+                        
+                        name: "Gigapipe",
+                        text: "Polyglot Observability",
+                        tagline: "Just Logs, Metrics, Traces & Profiles",
+                        actions: [
+                            { text: "About", link: "/about", theme: "alt" },
+                            { text: "Plans", link: "/pricing", theme: "alt" },
+                            { text: "Signup", link: "/signup", theme: "brand" },
+                        ],
+                        image: {
+                            src: "/logo.svg",
+                            alt: "Gigapipe",
+                        },
+                    },
+
+                }}
+                routePath="/"
+            />
+
+            <HomeFeature
+                frontmatter={{
+                    title: "Gigapipe Features",
+                    titleSuffix: "Docs Main Test",
+                    features: [
+                        {
+                            title: "Polyglot",
+                            details:
+                                "<p className='text-center'>Drop-in Loki, Prometheus, Tempo, Pyroscope compatible</p>",
+                            icon: "<img src='https://raw.githubusercontent.com/gigapipehq/homepage/refs/heads/main/docs/public/qryn.png' />",
+                            span: 3,
+                        },
+                        {
+                            title: "Performant",
+                            details:
+                                "<p className='text-center'>ClickHouse + DuckDB OLAP<br>w/ NVMe lightspeed storage</p>",
+                            icon: "⚡",
+                            span: 3,
+                        },
+
+                        {
+                          title: "Unmetered",
+                          details: "<p className='text-center'>Flat cost Observability<br>Zero monthly suprises</p>",
+                          icon: "💸",
+                          span: 3,
+                        },
+
+                        {
+                          title: "Opensource",
+                          details: "<p className='text-center'>Transparent, Open-Source,<br>Opentelemetry Native</p>",
+                          icon:"⚗️",
+                          span: 3
+                        },
+
+                    ],
+                }}
+                routePath="/"
+            />
+
+        </div>
+    );
+};
+
+export default Docs;
